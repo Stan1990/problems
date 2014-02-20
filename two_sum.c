a6a5985f25100c0a2554926254ecfebdb147d230
zhouxiaoqingelse <zhouxiaoqingelse@163.com>
2014-02-20 09:35:51 +0800

    structure add

---
+++ two_sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdblib.h>
+
+//   从数组中找到两个数使其和能等于给出的数，两个数中的
+// 一个数已经给出
+
+// 1. hash table
+// 2. bitmap	<= using this solution
+
+int
+main(void) {
+	
+
+	return 0;
+}
+
