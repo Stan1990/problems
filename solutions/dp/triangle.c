287dcf1e4fd419fdbbb442de492d522c31a6305a
zhouxiaoqingelse <zhouxiaoqingelse@163.com>
2014-03-27 09:43:14 +0800

    modify dir structure

---
+++ solutions/dp/triangle.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 三角问题
+// 	Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+// 	For example, given the following triangle
+// 	[
+// 	     [2],
+// 	    [3,4],
+// 	   [6,5,7],
+// 	  [4,1,8,3]
+// 	]
+//
+// 	2->3->5->1
+
+void
+triangle(int **heap, int level) {
+	
+}
+
+void
+solution() {
+	int n = 4;		// level
+	int i;
+	for (i=0; i<4; i++) {
+		printf(" %d \n", n);
+	}
+}
+
+int
+main(int argc, char *argv[]) {
+	solution();
+	return 0;
+}
